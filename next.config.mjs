ac41b75c568c6e650a46bc5eb7d867b4be2a41ff
vighnesh09 <vighneshparmar09@gmail.com>
2025-08-17 14:58:31 +0530

    fixed alian and change in next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, 'src')
-    }
-    return config
-  }
-}
+const nextConfig = {};
 
 export default nextConfig;
